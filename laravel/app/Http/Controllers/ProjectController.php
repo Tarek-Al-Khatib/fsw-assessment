9a6aec7e8975b9f6568824912c3e67204bd7f374
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-02 10:57:35 +0200

    Created get functions in project controller

--- laravel/app/Http/Controllers/ProjectController.php
+++ laravel/app/Http/Controllers/ProjectController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function getProjects(){
+        $projects = Project::all();
+        return response()->json([$projects]);
+    }
+
+
+    public function getProjectOfUser($id){
+        $user = User::find($id);
+
+        if(!$user){
+            return response()->json(["message" => "You do not exist"]);
+        }
+
+        $projects = Project::where("user_id", "=", $user->id);
+
+        return response()->json($projects);
+    }
+
+
 }
