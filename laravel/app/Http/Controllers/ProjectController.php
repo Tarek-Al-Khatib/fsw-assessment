403a987df3b08dab524cb8a545014949ce5faa8b
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-02 12:19:46 +0200

    Fixing members issue

--- laravel/app/Http/Controllers/ProjectController.php
+++ laravel/app/Http/Controllers/ProjectController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function getProjects(){
-        $projects = Project::all();
+        $projects = Project::with('members')->get();
         return response()->json([$projects]);
     }
 
--- laravel/app/Models/Project.php
+++ laravel/app/Models/Project.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     protected $fillable = ["name" , "description"];
+
+
+    public function members()
+    {
+        return $this->belongsToMany(User::class, "members", "project_id", "user_id");
+    }
 }
