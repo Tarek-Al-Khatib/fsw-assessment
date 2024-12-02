6da5a3179fcfd2baec2b4c93162a7028cbfb96f0
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-12-02 12:04:48 +0200

    Fixed bug on projectContext

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,16 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Projects from "./pages/Projects";
 import Auth from "./pages/Auth";
-
+import ProjectProvider from "./context/ProjectContext";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Auth />}></Route>
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </BrowserRouter>
+      <ProjectProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Auth />}></Route>
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </BrowserRouter>
+      </ProjectProvider>
     </div>
   );
 }
--- react-app/src/context/ProjectContext.jsx
+++ react-app/src/context/ProjectContext.jsx
@@ -1,43 +1,41 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
-
 export const projectContext = createContext();
+
 const ProjectProvider = ({ children }) => {
   const [projects, setProjects] = useState([]);
   const [personalProjects, setPersonalProjects] = useState([]);
+  const [userId, setUserId] = useState(null);
 
   async function fetchProjects() {
     const response = await axios.get("http://127.0.0.1:8000/api/projects");
-    return response.data;
+    setProjects(response.data);
   }
 
   async function fetchPersonalProjects(id) {
+    if (!id) return;
     const response = await axios.get(
-      "http://127.0.0.1:8000/api/projects/user/" + id
+      `http://127.0.0.1:8000/api/projects/user/${id}`
     );
-    return response.data;
+    setPersonalProjects(response.data);
   }
 
   async function addProject(project) {
-    const response = await axios.post(
-      "http://127.0.0.1:8000/api/projects/",
-      project
-    );
-    return response.data;
+    await axios.post("http://127.0.0.1:8000/api/projects/", project);
+    fetchProjects();
+    if (userId) fetchPersonalProjects(userId);
   }
 
   async function updateProject(id, project) {
-    const response = await axios.put(
-      "http://127.0.0.1:8000/api/projects/" + id,
-      project
-    );
-    return response.data;
+    await axios.put(`http://127.0.0.1:8000/api/projects/${id}`, project);
+    fetchProjects();
+    if (userId) fetchPersonalProjects(userId);
   }
 
   useEffect(() => {
     fetchProjects();
-    fetchPersonalProjects();
-  }, []);
+    if (userId) fetchPersonalProjects(userId);
+  }, [userId]);
 
   return (
     <projectContext.Provider
@@ -48,9 +46,12 @@ const ProjectProvider = ({ children }) => {
         fetchPersonalProjects,
         addProject,
         updateProject,
+        setUserId,
       }}
     >
       {children}
     </projectContext.Provider>
   );
 };
+
+export default ProjectProvider;
